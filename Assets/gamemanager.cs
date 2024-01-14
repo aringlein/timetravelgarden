3961139c4bf210fdfa0e479f7398037388a03918
Alex Ringlein <alex.ringlein@gmail.com>
2024-01-14 13:07:47 -0800

    display days

--- Assets/gamemanager.cs
+++ Assets/gamemanager.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Math = System.Math;
+using UnityEngine.UI;
+using TMPro;
 
 public class gamemanager : MonoBehaviour
 {
     public GameObject lemonTreePrefab;
 
+    public Slider slider;
+
+    public TextMeshProUGUI clock;
+
+    private ArrayList lemonTrees = new ArrayList();
+
 
     public int GRID_SIZE = 4;
+    public int PLANE_SIZE = 6;
+
+    public float daysPerTimeUnit = 1.0f;
+
+    public float currentTime = 0; // units: TIME
     // Start is called before the first frame update
     void Start()
     {
-
+        currentTime = 0; //Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    void spawnTreeOnClick()
     {
         // Spawn a cube when mouse is clicked, where the mouse intersects with the plane
         if (Input.GetMouseButtonDown(0))
@@ -31,15 +43,13 @@ public class gamemanager : MonoBehaviour
             {
                 // Instantiate a lemon tree prefab at the intersection point
 
-
-
-
                 // // Instantiate a cube at the intersection point
 
                 //  GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 Vector3 candidatePoint = ray.GetPoint(distance);
                 int x = (int)Math.Round((candidatePoint.x) / GRID_SIZE);
                 int z = (int)Math.Round((candidatePoint.z) / GRID_SIZE);
+                if (x < -PLANE_SIZE || x > PLANE_SIZE || z > PLANE_SIZE || z < -PLANE_SIZE) return;
                 Vector3 finalPoint = new Vector3(x * GRID_SIZE, candidatePoint.y, z * GRID_SIZE);
 
                 // Debug.Log("finalPoint: " + finalPoint);
@@ -49,8 +59,26 @@ public class gamemanager : MonoBehaviour
 
                 GameObject lemonTree = Instantiate(lemonTreePrefab, finalPoint, Quaternion.identity);
 
+                lemonTrees.Add(lemonTree);
+
             }
         }
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTreeOnClick();
+
+        // Update current time
+        currentTime = currentTime + (Time.deltaTime) * slider.value;
+        float daysToDisplay = currentTime * (daysPerTimeUnit);
+        // convert floating point days to hours, minutes, seconds
+        int daysInt = (int)Math.Floor(daysToDisplay);
+        float hoursToDisplay = (daysToDisplay - daysInt) * 24;
+        int hours = (int)Math.Floor(hoursToDisplay);
+        //float minutesToDisplay = (hoursToDisplay - hours) * 60;
+        //int minutes = (int)Math.Floor(minutesToDisplay);
+        clock.text = daysInt + " days, " + hours + " hours";
     }
 }
--- Assets/growingtree.cs
+++ Assets/growingtree.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Math = System.Math;
 
 public class growingtree : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class growingtree : MonoBehaviour
 
     private GameObject ownedTree;
 
+    public GameObject gameManager;
+
     private enum TreeState
     {
         Young,
@@ -22,27 +25,63 @@ public class growingtree : MonoBehaviour
     private TreeState treeState;
 
 
+
+    public float SCALE_RATE = 0.01f;
+    public float MAX_SCALE = 2.0f;
+    // public float MIN_SCALE = 1.0f;
+
+    private float getCurrentTime()
+    {
+        return gameManager.GetComponent<gamemanager>().currentTime;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("GameManager");
         // Instantiate a lemon tree prefab at the intersection point
         ownedTree = Instantiate(lemonTreePrefabYoung, transform.position, Quaternion.identity);
-        birthTime = Time.time;
+        birthTime = getCurrentTime();
         treeState = TreeState.Young;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float currentTime = getCurrentTime();
+        // Scale the tree based on time
+        float scale = 1 + Math.Min((float)(currentTime - birthTime) * SCALE_RATE, MAX_SCALE);
+        ownedTree.transform.localScale = new Vector3(scale, scale, scale);
+
+        float timeSinceBirth = (float)(currentTime - birthTime);
+
+
+        if (timeSinceBirth < 0)
+        {
+            ownedTree.active = false;
+        }
+        else
+        {
+            ownedTree.active = true;
+        }
+
         // Change the treeState from young to middle to old if needed, based on the current time
         // If the treeState changes, destroy ownedTree and reinstantiate it with the new prefab
-        if (Time.time - birthTime > timeToBeYoung && treeState == TreeState.Young)
+        if (timeSinceBirth <= timeToBeYoung && treeState != TreeState.Young)
+        {
+            treeState = TreeState.Young;
+            Destroy(ownedTree);
+            ownedTree = Instantiate(lemonTreePrefabYoung, transform.position, Quaternion.identity);
+        }
+        else if (timeSinceBirth > timeToBeYoung && timeSinceBirth <= timeToBeYoung + timeToBeMiddle && treeState != TreeState.Middle)
         {
             treeState = TreeState.Middle;
             Destroy(ownedTree);
             ownedTree = Instantiate(lemonTreePrefabMiddle, transform.position, Quaternion.identity);
         }
-        else if (Time.time - birthTime > timeToBeMiddle && treeState == TreeState.Middle)
+        else if (timeSinceBirth > timeToBeMiddle + timeToBeYoung && treeState != TreeState.Old)
         {
             treeState = TreeState.Old;
             Destroy(ownedTree);
